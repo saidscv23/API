6547e0740801bbe61751ecc6348934f6d9d0ab21
saidscv23 <said.carrillo438@ist17dejulio.edu.ec>
2024-01-29 12:05:10 -0500

    1 implementacion de webSer;

--- app/src/main/java/com/example/webservice/MainActivity.java
+++ app/src/main/java/com/example/webservice/MainActivity.java
@@ -3,12 +3,80 @@ package com.example.webservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edNum1, edNum2;
+    Button btprocesar;
+    TextView tvresultado;
+String Respuesta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edNum1 = findViewById(R.id.num1);
+        edNum2 = findViewById(R.id.num2);
+        btprocesar = findViewById(R.id.btn_Re);
+        tvresultado = findViewById(R.id.resultado);
+
+        btprocesar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConsumirApi();
+            }
+        });
+
     }
+        private void ConsumirApi () {
+
+String pro="https://ejemplo2apimovil20240128220859.azurewebsites.net/api/Operaciones?a=4&b=7";
+OkHttpClient cliente=new OkHttpClient();
+
+            Request get=new Request.Builder().url(pro).build();
+
+
+            cliente.newCall(get).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+try{
+
+    ResponseBody responseBody=response.body();
+ if(response.isSuccessful()){
+     throw new IOException("Respuesta inesperada"+response);
+
+ }
+ Respuesta=responseBody.string();
+
+}catch (Exception e){
+
 }
+
+
+
+                }
+            });
+
+        }
+
+
+    }
+
+
